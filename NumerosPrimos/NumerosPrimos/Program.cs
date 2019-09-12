6010b27cb189c015e9d8cd23951c043a8880002b
Douglas Cuco <douglas.cuco@gmail.com>
2019-09-12 08:12:34 -0300

    Numeros primos PRONTO.

--- NumerosPrimos/NumerosPrimos/Program.cs
+++ NumerosPrimos/NumerosPrimos/Program.cs
@@ -14,29 +14,27 @@ namespace NumerosPrimos
             int max = int.Parse(Console.ReadLine());
             // var ultimoPrimo = 3;
             int cont = 0;
+            bool ePrimo = true;
             for (int i = 3; i <= max; i++)
-            {
-               
+            {             
                 if (i % 2 != 0) //tira os numeros pares
                 {
-                    
+                    ePrimo = true;
                     int impar = i;
                     for (int j = 2; j < impar; j++)
-                    {
-                            
+                    {                           
                         if (impar % j == 0)
                         {
                             int naoPrimo = impar;  
-                           
-                        }                           
-                          
+                            ePrimo = false;
+                        }                                                     
                     }
-                        
-
+                    if(ePrimo == true)
+                        Console.WriteLine(impar +" é um numero Primo");
+                        cont ++;
                 }
-
-                
             }
+            Console.WriteLine("Existe "+cont+" numeros primos até o numero "+max);
             Console.ReadKey();
         }
     }

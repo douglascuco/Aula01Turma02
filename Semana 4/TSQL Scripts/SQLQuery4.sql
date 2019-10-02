042d45f39e58de3d5affaef906fe1686991b3cdd
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-02 17:22:17 -0300

    sql group by

---
+++ Semana 4/TSQL Scripts/SQLQuery4.sql
@@ -0,0 +1,4 @@
+﻿insert into Funcionarios (Nome, DataInicio, DataSaida) values ('Joservaldo','01/12/2018','05/30/2019'),('Gina','02/28/2015','10/02/2019'),('Andreia','10/02/2019','02/10/2020')
+
+insert into Salarios (FuncionarioId, Valor, DataCriacao) values (1, 2569.45, '08/05/2019'),(2, 6102.6, '08/05/2019'),(3, 1510.99, '08/05/2019'),
+																(1, 2700.45, '09/05/2019'),(2, 7548.6, '09/05/2019'),(3, 1521.99, '09/05/2019')

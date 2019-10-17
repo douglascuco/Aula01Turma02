bec0398e41c3a64804768fb70fe12cf104aa4175
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-17 17:29:34 -0300

    sistema monetario, arrumando historico

--- Semana 6/ConversorDeMoedas/js/scripts.js
+++ Semana 6/ConversorDeMoedas/js/scripts.js
@@ -8,20 +8,56 @@ $(document).ready(
             return "$ " + s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
         }
 
-
-
-
-
-
-        $('input[name="dolar"]').mask("R$ 9999999.99")
+        $('input[name="real"]').mask("R$ 999999999.99")
+        var count = 1;
         $('input[name="converter"]').click(
             function () {
-                var dolar = $('input[name="dolar"]').val();
-                dolar = dolar.replace("R$", "");
-                dolar = dolar.replace(" ", "");
-                var real = dolar * 4.18;
-                $('input[name="resultado"]').val(number_format(real, 2, ',', '.'));
+                var real = $('input[name="real"]').val();
+                real = real.replace("R$ ", "");
+                var dolar = real / 4.18;
+                $('input[name="resultado"]').val(number_format(dolar, 2, '.', ','));
 
-            })
+                var objTo = document.getElementById('card');
+                var div_lista = document.createElement("div");
+                var div_lista2 = document.createElement("div");
+                div_lista.classList.add("row", "form-group");
+                div_lista2.classList.add("col-md-2");
+                var text_lista = document.createTextNode(count++);
+                div_lista2.appendChild(text_lista);
+                div_lista.appendChild(div_lista2);
+
+
+                var div_money1 = document.createElement("div");
+                div_money1.classList.add("col-md-3");
+                var text_money1 = document.createTextNode($('input[name="real"]').val());
+                div_money1.appendChild(text_money1);
+                div_lista.appendChild(div_money1);
+                
+
+
+                var div_money2 = document.createElement("div");
+                div_money2.classList.add("col-md-3");
+                var text_money = document.createTextNode($('input[name="resultado"]').val());
+                div_money2.appendChild(text_money);
+                div_lista.appendChild(div_money2);
+
+                var div_data2 = document.createElement("div");
+                div_data2.classList.add("col-md-4");
+                /*function dataAgora() {
+                    var data = new Date();
+                    var data_atual = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds();
+                    return data_atual;
+                }*/
+                var data = new Date();
+                var text_data = document.createTextNode(data.toLocaleString());
+                div_data2.appendChild(text_data);
+                div_lista.appendChild(div_data2);
+
+
+                objTo.appendChild(div_lista);
+
+
+            }
+        )
     }
 );

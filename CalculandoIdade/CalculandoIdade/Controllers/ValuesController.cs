6cf46cbe67a83acb85fd86b03cabf02e2b7bf395
Douglas Cuco <douglas.cuco@gmail.com>
2019-10-25 17:12:28 -0300

    Site Api modal

---
+++ CalculandoIdade/CalculandoIdade/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CalculandoIdade.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(string nome, int peso, int altura)
+        {
+            var imc = peso / (altura*altura);
+            return $"Olá {nome} seu imc é {imc} e ele foi calculado de acordo com sua Altura: {altura} e Peso:{peso}";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SiteParaApiImc/SiteParaApiImc/Utils/scripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#calcular').click(function () {
+        var obj = $("#formularioImc").serialize()
+        console.log(obj)
+
+        $.get("http://localhost:65170/api/CalculoIdade?" + obj, function (data) {
+
+            $('#resultado').text(data)
+
+        })
+    })
+})
